1f57534943d79f28dc25778eb3a03188e8a9630a
AmanVerma5 <118344057+AmanVerma5@users.noreply.github.com>
2023-06-06 14:22:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function getFormvalue() {
     //Write your code here
-
+	const form = document.getElementById("form1");
+  
+  // Get the input values
+  const firstName = form.fname.value;
+  const lastName = form.lname.value;
+  
+  // Display an alert with the first and last name
+  alert("First Name: " + firstName + "\nLast Name: " + lastName);
+  
+  // Prevent the form from submitting
+  return false;
 }
